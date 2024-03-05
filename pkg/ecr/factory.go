56ffb3e8864183abe387af4ad31836b749a879c9
Greg Furman <gregfurman99@gmail.com>
2024-03-05 19:15:09 +0200

    Add mocks

--- pkg/ecr/factory.go
+++ pkg/ecr/factory.go
@@ -14,7 +14,7 @@ type loadConfig func(ctx context.Context, optFns ...func(*config.LoadOptions) er
 func clientFromConfig(cfgLoader loadConfig, ctx context.Context, optFns ...func(*config.LoadOptions) error) *ecr.Client {
 	cfg, err := cfgLoader(ctx, optFns...)
 	if err != nil {
-		logrus.Fatalf("Failed to create ECR client from config: %w")
+		logrus.Fatalf("Failed to create ECR client from config: %s", err)
 		return nil
 
 	}
